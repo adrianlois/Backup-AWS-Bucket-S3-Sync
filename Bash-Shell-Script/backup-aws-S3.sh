98dc0dea829b35dcb27311b19495a2f7b3cea9cf
Adrian Lois <1989adrianlois@gmail.com>
2019-09-05 22:34:24 +0200

    backup-aws-S3.sh

--- Bash-Shell-Script/backup-aws-S3.sh
+++ Bash-Shell-Script/backup-aws-S3.sh
@@ -16,7 +16,7 @@ cuerpoEmail="cuerpoEmail"
 # Paths
 pathLocalDatos="/pathLocal/datos/"
 pathRemotoBucketS3="s3://bucketS3/backup/"
-backuplog="backup_$fechaActual.log"
+backupLog="backup_$fechaActual.log"
 
 # Comprobar si existen ficheros de log pasados del backup
 if [ -f "*backup*.log" ];
@@ -25,17 +25,21 @@ then
 fi
 
 # Mostrar fecha y hora del comienzo del proceso de backup al princpio del log
-echo "El backup comienza: $fechaHoraActual" > $backuplog
-echo -e "# # # # # # # # # # # # # # # # # # # # #\n" >> $backuplog
+echo "El backup comienza: $fechaHoraActual" > $backupLog
+echo -e "# # # # # # # # # # # # # # # # # # # # #\n" >> $backupLog
 
 # Sincronizar datos locales a bucket S3 de AWS
-aws s3 sync $pathLocalDatos $pathRemotoBucketS3 --sse AES256 --delete --include "*" >> $backuplog
+aws s3 sync $pathLocalDatos $pathRemotoBucketS3 --sse AES256 --delete --include "*" >> $backupLog
 
-echo -e "\n# # # # # # # # # # # # # # # # # # # # #" >> $backuplog
+echo -e "\n# # # # # # # # # # # # # # # # # # # # #" >> $backupLog
 # Mostrar fecha y hora de la finalización del proceso de backup al final del log
 # Resetear la variable fechaHoraActual para obtener la hora actual hasta este momento del proceso de backup
 fechaHoraActual="$(date +'%d/%m/%Y - %H:%M:%S')"
-echo "El backup finaliza: $fechaHoraActual" >> $backuplog
+echo "El backup finaliza: $fechaHoraActual" >> $backupLog
 
-# Envío del fichero log adjunto vía Email usando el comando mail.
-echo "$cuerpoEmail - $fechaHoraActual" | mail -s "$asuntoEmail" "$envioEmailCuentaUsuario" -A "$backuplog"
+# Elegir una opción de envío
+
+# a) Envío del fichero log adjunto vía Email usando el comando mail.
+echo "$cuerpoEmail" | mail -s "$asuntoEmail" "$envioEmailCuentaUsuario" -A "$backupLog"
+# b) Envío del contenido del fichero log en el cuerpo del Email usando el comando mail.
+cat "$backupLog" | mail -s "$asuntoEmail" "$envioEmailCuentaUsuario"
