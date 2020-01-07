1be21e4a43213bd2bb1aa5dc303a9c3c256c0e1f
Adrian Lois <34519511+adrianlois@users.noreply.github.com>
2020-01-07 23:41:22 +0100

    call-ps1-backup-aws-S3.bat

--- PowerShell/call-ps1-backup-aws-S3.bat
+++ PowerShell/call-ps1-backup-aws-S3.bat
@@ -1,6 +1,6 @@
 @echo off
 
-:: Llamada a fichero ps1 desde un fichero bat
+:: Llamada al fichero ps1 desde un fichero bat
 set pathLocalPs1="pathLocalFichero.ps1"
 powershell.exe -file %pathLocalPs1%
 
