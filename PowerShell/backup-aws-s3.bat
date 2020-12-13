550897b1930c0619d7495c85b520ec4922f24225
Adrian Lois <34519511+adrianlois@users.noreply.github.com>
2020-12-13 12:49:18 +0100

    Update and rename call-ps1-backup-aws-S3.bat to backup-aws-s3.bat

--- PowerShell/backup-aws-s3.bat
+++ PowerShell/backup-aws-s3.bat
@@ -2,6 +2,6 @@
 
 :: Llamada al fichero ps1 desde un fichero bat
 set pathLocalPs1="pathLocalFichero.ps1"
-powershell.exe -file %pathLocalPs1%
+powershell.exe -ExecutionPolicy Bypass -File %pathLocalPs1%
 
 exit
