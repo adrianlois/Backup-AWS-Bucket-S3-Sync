3085613a7318a8c229d57eafb9e6a6454434bfd1
Adrian Lois <34519511+adrianlois@users.noreply.github.com>
2021-05-01 19:17:26 +0200

    MountDismountUSB scripts

---
+++ PowerShell/MountDismountUSB/MountDismountUSB_backup.bat
@@ -0,0 +1,4 @@
+@echo off
+mountvol P:\ \\?\Volume{2a3fec37-0000-0000-0000-100000000000}
+timeout 2400
+mountvol P:\ /p
---
+++ PowerShell/MountDismountUSB/MountDismountUSB_backup.ps1
@@ -0,0 +1,15 @@
+Function MountDismountUSB {
+
+	param (
+		[Parameter(Mandatory=$True)]
+		[string]$dl,
+		[Parameter(Mandatory=$True)]
+		[string]$s
+	)
+
+	$id = (Get-Volume | Where-Object {$_.DriveLetter -eq "$dl"}).UniqueId
+	mountvol $dl+":\" $id
+	Start-Sleep -Seconds $s
+	mountvol $dl+":\" /p
+}
+MountDismountUSB -dl "P" -s "2400"
