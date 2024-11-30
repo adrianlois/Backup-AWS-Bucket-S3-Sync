8e4e3711b4f0e94601910de24df8369885ce6120
Adrian Lois <34519511+adrianlois@users.noreply.github.com>
2024-12-01 00:23:49 +0100

    update changes veracrypt

--- PowerShell/USBDrive-MountUnmount/Invoke-USBDriveMountUnmount-Trigger.bat
+++ PowerShell/USBDrive-MountUnmount/Invoke-USBDriveMountUnmount-Trigger.bat
@@ -1,6 +1,6 @@
 @echo off
 :: Llamada al fichero ps1 desde un fichero bat usado para el disparador de la tarea programada (taskschd.msc).
 :: El script USBDrive-MountUnmount.ps1 será necesario ejecutarlo en una tarea programada en un contexto de un usuario privilegiado.
+:: powershell.exe -WindowStyle Hidden -ExecutionPolicy Bypass -File "C:\PATH\Invoke-USBDriveMountUnmount.ps1"
 pwsh.exe -WindowStyle Hidden -ExecutionPolicy Bypass -File "C:\PATH\Invoke-USBDriveMountUnmount.ps1"
-:: powershell.exe -WindowStyle Hidden -ExecutionPolicy Bypass -File "C:\PATH\USBDrive-MountUnmount.ps1"
 exit
--- PowerShell/USBDrive-MountUnmount/USBDrive-UnmountStartSystem.bat
+++ PowerShell/USBDrive-MountUnmount/USBDrive-UnmountStartSystem.bat
@@ -1,5 +1,5 @@
 @echo off
-:: Este script se ejecutará en una tarea programada cada vez que inicie el sistema.
-:: Sutituir E: por la letra de asignación del disco a desmontar en el arranque del sistema.
-mountvol "E:" /P
+:: Este script se ejecutará en una tarea programada cada vez que inicie el sistema o en el primer inicio de sesión.
+:: Sutituir X: por la letra de asignación del disco a desmontar en el arranque del sistema.
+mountvol "X:" /P
 exit
